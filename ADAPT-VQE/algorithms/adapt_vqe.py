58b1aff221512f657267f7a860c06562f790be3c
Azhar Ikhtiarudin <azharikhtiarudin@gmail.com>
2024-11-11 17:32:22 +0700

    add adapt-vqe code

---
+++ ADAPT-VQE/algorithms/adapt_vqe.py
@@ -0,0 +1,4 @@
+
+class AdaptVQE():
+    def __init__(self):
+        pass
---
+++ ADAPT-VQE/src/utilities.py
@@ -0,0 +1,241 @@
+
+import numpy as np
+from openfermion import (jordan_wigner,
+                         QubitOperator)
+
+
+def get_operator_qubits(operator):
+    """
+    Obtains the support of an operator.
+
+    Args:
+        operator (Union[FermionOperator, QubitOperator]): the operator in question
+
+    Returns:
+        qubits (Set): List containing the indices of the qubits in which operator acts on non-trivially
+    """
+    qubits = set()
+
+    for string in list(operator.terms.keys()):
+        for qubit, pauli in string:
+            if qubit not in qubits:
+                qubits.add(qubit)
+
+    return qubits
+
+
+def remove_z_string(operator):
+    """
+    Removes the anticommutation string from Jordan-Wigner transformed excitations. This is equivalent to removing
+    all Z operators.
+    This function does not change the original operator.
+
+    Args:
+        operator (Union[FermionOperator, QubitOperator]): the operator in question
+
+    Returns:
+        new_operator (Union[FermionOperator, QubitOperator]): the same operator, with Pauli-Zs removed
+    """
+
+    if isinstance(operator, QubitOperator):
+        qubit_operator = operator
+    else:
+        qubit_operator = jordan_wigner(operator)
+
+    new_operator = QubitOperator()
+
+    for term in qubit_operator.get_operators():
+
+        coefficient = list(term.terms.values())[0]
+        pauli_string = list(term.terms.keys())[0]
+
+        new_pauli = QubitOperator((), coefficient)
+
+        for qubit, operator in pauli_string:
+            if operator != 'Z':
+                new_pauli *= QubitOperator((qubit, operator))
+
+        new_operator += new_pauli
+
+    return new_operator
+
+
+def cnot_depth(qasm, n):
+    """
+    Counts the depth of a circuit on n qubits represented by a QASM string, considering only cx gates.
+    Circuit must be decomposed into a cx + single qubit rotations gate set.
+
+    Aguments:
+        qasm (str): the QASM representation of the circuit
+        n (int): the number of qubits
+    Returns:
+        The CNOT depth of the circuit
+    """
+    # n = int(re.search(r"(?<=q\[)[0-9]+(?=\])", qasm.splitlines()[2]).group())
+    depths = [0 for _ in range(n)]
+
+    for line in qasm.splitlines()[3:]:
+        # Remove ;
+        line = line[:-1]
+
+        # Split line by spaces
+        line_elems = line.split(" ")
+
+        # First element is operation type
+        op = line_elems[0]
+        if op[:2] != "cx":
+            continue
+
+        # Next elements are qubits
+        qubits = [
+            int(re.search(r"[0-9]+", qubit_string).group())
+            for qubit_string in line_elems[1:]
+        ]
+
+        max_depth = max([depths[qubit] for qubit in qubits])
+        new_depth = max_depth + 1
+
+        for qubit in qubits:
+            depths[qubit] = new_depth
+
+    return max(depths)
+
+
+def cnot_count(qasm):
+    """
+    Counts the CNOTs in a circuit represented by a QASM string.
+    """
+    count = 0
+
+    for line in qasm.splitlines()[3:]:
+        # Remove ;
+        line = line[:-1]
+        line_elems = line.split(" ")
+        op = line_elems[0]
+
+        if op[:2] == "cx":
+            count += 1
+
+    return count
+
+
+
+
+def qe_circuit(source_orbs, target_orbs, theta, n, big_endian=False):
+    """
+    Creates a qubit excitation circuit. See https://doi.org/10.1103/PhysRevA.102.062612
+
+    Arguments:
+        source_orbs (list): the spin-orbitals from which the excitation removes electrons
+        target_orbs (list): the spin-orbitals to which the excitation adds electrons
+        theta (float): the coefficient of the excitation
+        n (int): the number of qubits
+        big_endian (bool): if True/False, big/little endian ordering will be assumed
+
+    Returns:
+        QuantumCircuit (the circuit implementing the operator in Qiskit)
+    """
+
+    if len(source_orbs) == 2:
+        return double_qe_circuit(source_orbs, target_orbs, theta, n, big_endian)
+    else:
+        return single_qe_circuit(source_orbs, target_orbs, theta, n, big_endian)
+
+
+def double_qe_circuit(source_orbs, target_orbs, theta, n, big_endian=False):
+    """
+    Creates a qubit excitation circuit. See https://doi.org/10.1103/PhysRevA.102.062612
+
+    Arguments:
+        source_orbs (list): the spin-orbitals from which the excitation removes electrons
+        target_orbs (list): the spin-orbitals to which the excitation adds electrons
+        theta (float): the coefficient of the excitation
+        n (int): the number of qubits
+        big_endian (bool): if True/False, big/little endian ordering will be assumed
+
+    Returns:
+        QuantumCircuit (the circuit implementing the operator in Qiskit)
+    """
+
+    a, b = source_orbs
+    c, d = target_orbs
+
+    if big_endian:
+        # Qiskit's default is little endian - switch
+        a = n - a - 1
+        b = n - b - 1
+        c = n - c - 1
+        d = n - d - 1
+
+    qc = QuantumCircuit(n)
+
+    qc.cx(a, b)
+    qc.cx(c, d)
+    qc.x(b)
+    qc.x(d)
+    qc.cx(a, c)
+    qc.ry(2 * theta / 8, a)
+
+    qc.h(b)
+    qc.cx(a, b)
+    qc.h(d)
+    qc.ry(-2 * theta / 8, a)
+
+    qc.cx(a, d)
+    qc.ry(2 * theta / 8, a)
+
+    qc.cx(a, b)
+    qc.h(c)
+    qc.ry(-2 * theta / 8, a)
+
+    qc.cx(a, c)
+    qc.ry(2 * theta / 8, a)
+
+    qc.cx(a, b)
+    qc.ry(-2 * theta / 8, a)
+
+    qc.cx(a, d)
+    qc.ry(2 * theta / 8, a)
+
+    qc.cx(a, b)
+    qc.ry(-2 * theta / 8, a)
+
+    qc.h(d)
+    qc.h(b)
+    qc.rz(+np.pi / 2, c)
+    qc.cx(a, c)
+
+    qc.rz(-np.pi / 2, a)
+    qc.rz(+np.pi / 2, c)
+    qc.ry(+np.pi / 2, c)
+
+    qc.x(b)
+    qc.x(d)
+    qc.cx(a, b)
+    qc.cx(c, d)
+
+    return qc
+
+def normalize_op(operator):
+    """
+    Normalize Qubit or Fermion Operator by forcing the absolute values of the coefficients to sum to zero.
+    This function modifies the operator.
+
+    Arguments:
+        operator (Union[FermionOperator,QubitOperator]): the operator to normalize
+
+    Returns:
+        operator (Union[FermionOperator,QubitOperator]): the same operator, now normalized0
+    """
+
+    if operator:
+        coeff = 0
+        for t in operator.terms:
+            coeff_t = operator.terms[t]
+            # coeff += np.abs(coeff_t * coeff_t)
+            coeff += np.abs(coeff_t)
+
+        # operator = operator/np.sqrt(coeff)
+        operator = operator / coeff
+
+    return operator
