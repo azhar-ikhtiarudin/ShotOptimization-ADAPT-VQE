4211f62d77a1445dfcf2998252efa1cad3e5abfb
Azhar Ikhtiarudin <azharikhtiarudin@gmail.com>
2025-02-12 14:09:49 +0700

    add gradient commuting example

--- main_h2_testing.py
+++ main_h2_testing.py
@@ -1,31 +1,36 @@
 from src.pools import QE
-from src.molecules import create_h2, create_h3, create_h4
+from src.molecules import create_h2, create_h3, create_lih
+from src.hamiltonian import h_lih
+from src.utilities import to_qiskit_operator
+from algorithms.adapt_vqe_v8 import AdaptVQE
 
-from algorithms.adapt_vqe_v3 import AdaptVQE
+from openfermion.transforms import jordan_wigner
+from openfermion.utils import commutator
+
+from qiskit.quantum_info import SparsePauliOp
 
 
 if __name__ == '__main__':    
-    r = 1.75
-    molecule = create_h3(r)
-    pool = QE(molecule)
+    r = 0.742
+    molecule = create_h2(r)
+    
+    # Hamiltonian
+    fermionic_hamiltonian = molecule.get_molecular_hamiltonian()
+    qubit_hamiltonian = jordan_wigner(fermionic_hamiltonian)
+    # print(f"Qubit Hamiltonian:", qubit_hamiltonian)
 
-    print(len(pool.operators))
+    # Pool
+    pool = QE(molecule=molecule)
+    gradient_list = []
+
+    # print("Pool:", pool)
     for i in range(len(pool.operators)):
-        print(type(pool.operators[i]))
-        print(pool.operators[i].source_orbs)
-        print(pool.operators[i].target_orbs)
+        print(f"\nPool-{i}")
+        # print("Pool:", pool.operators[i].q_operator)
+        gradient = commutator(qubit_hamiltonian, pool.operators[i].q_operator)
+        gradient_qiskit = to_qiskit_operator(gradient)
 
-    # adapt_vqe = AdaptVQE(pool=pool,
-    #                     molecule=molecule,
-    #                     max_adapt_iter=10,
-    #                     max_opt_iter=100,
-    #                     grad_threshold=0.01,
-    #                     vrb=True,
-    #                     optimizer_method='l-bfgs-b',
-    #                     shots_assignment='uniform',
-    #                     k=100,
-    #                     shots_budget=10000,
-    #                     N_experiments=2
-    #                     )
-
-    # adapt_vqe.run()
+        gradient_list.append(gradient_qiskit)
+    
+    print(gradient_list)
+        
